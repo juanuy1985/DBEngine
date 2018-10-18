2aa0e736da034cc3a45646b416b1f0b5cea6f83e
Ubuntu <ubuntu@ip-172-31-30-87.eu-west-1.compute.internal>
2018-10-18 23:39:39 +0000

    si

--- generateFeatures.cpp
+++ generateFeatures.cpp
@@ -7,7 +7,7 @@ using namespace cimg_library;
 int main()
 {
         string fileName = "/home/ubuntu/Proyectos/DBEngine/Resources/faces/adhast/adhast.1.jpg";
-        CImg<float>* image = new CImg<float>(fileName.c_str());
+        CImg<unsigned char>* image = new CImg<unsigned char>(fileName.c_str());
         image->resize(256, 256);
         // convert from unsigned char to float to support larger range of values
 
